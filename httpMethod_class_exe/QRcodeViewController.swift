e752bdb892fb308a06de28b520cf9f65e6f2a2dc
JiaHung Hsia <x120565903@gmail.com>
2019-07-17 11:56:48 +0800

    QQ

--- httpMethod_class_exe/QRcodeViewController.swift
+++ httpMethod_class_exe/QRcodeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 class QRcodeViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
-    
+    //**
     
 
     @IBOutlet weak var view_iv: UIImageView!
